c629fe522819f45e1fff94937fbd6735188847fe
Jodecir Neto <jodecirneto@gmail.com>
2021-11-25 16:01:10 -0300

    UPDATE spaces in factorial

--- src/course/com/dio/repetição/Fatorial.java
+++ src/course/com/dio/repetição/Fatorial.java
@@ -3,32 +3,32 @@ package course.com.dio.repetição;
 import java.util.Scanner;
 
 public class Fatorial {
-  public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-    boolean fechado = false;
-    String repetir;
+        boolean fechado = false;
+        String repetir;
 
-    do {
-      System.out.println("Fatorial: ");
-      int fatorial = scan.nextInt();
+        do {
+            System.out.println("Fatorial: ");
+            int fatorial = scan.nextInt();
 
-      int multiplicacao = 1;
+            int multiplicacao = 1;
 
-      System.out.print(fatorial +"! = ");
-      for(int i = fatorial ; i >= 1 ; i --) {
-          multiplicacao = multiplicacao * i;
-      }
+            System.out.print(fatorial +"! = ");
+            for(int i = fatorial ; i >= 1 ; i --) {
+                multiplicacao = multiplicacao * i;
+            }
 
-      System.out.println(multiplicacao);
+            System.out.println(multiplicacao);
 
-      System.out.println("Deseja continuar (s/n): ");
-      repetir = scan.next();
-      if (repetir.equals("n")) {
-          System.out.println("Finalizado aqui...");
-          fechado = true;
-      }
-    } while (fechado == false);
-    scan.close();
-  }
+            System.out.println("Deseja continuar (s/n): ");
+            repetir = scan.next();
+            if (repetir.equals("n")) {
+                System.out.println("Finalizado aqui...");
+                fechado = true;
+            }
+        } while (fechado == false);
+        scan.close();
+    }
 }
