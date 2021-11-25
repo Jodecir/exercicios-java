f4e2b44669d48972ef096ac50c94e845588de41d
Jodecir Neto <jodecirneto@gmail.com>
2021-11-25 15:00:09 -0300

    CREATE simple array project

---
+++ src/course/com/dio/array/OrdemInversa.java
@@ -0,0 +1,19 @@
+package course.com.dio.array;
+
+import java.util.Scanner;
+
+public class OrdemInversa {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int[] vetor = {-5, -6, 15, 50, 8, 4};
+
+        System.out.print("Vetor: ");
+        int count = 0;
+        while(count < (vetor.length)) {
+            System.out.print(vetor[count] + " ");
+            count++;
+        }
+        scan.close();
+    }
+}
