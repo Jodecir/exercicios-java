cd66542dd29f69f9ab065ab636e3e41e5f08f581
Jodecir Neto <jodecirneto@gmail.com>
2022-01-04 14:51:33 -0300

    CREATE temperature conversor

---
+++ src/course/com/dio/repetição/ConversorDeTemperatura.java
@@ -0,0 +1,60 @@
+package course.com.dio.repetição;
+
+import java.util.Scanner;
+
+public class ConversorDeTemperatura {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n1;
+        String escala = "c";
+
+        boolean fechado = false;
+        String repetir;
+
+        do {
+            System.out.println("Insira o valor: ");
+            n1 = scan.nextInt();
+
+            System.out.println("Insira a escala que ele está entre (F/C/K): ");
+            escala = scan.next();
+            
+            double ctof = (n1 * 1.8) + 32;
+            double ctok = n1 + 273.15;
+            double ftoc = (n1 - 32) / 1.8;
+            double ftok = ((n1 - 32) / 1.8) + 273.15;
+            double ktoc = n1 - 273.15;
+            double ktof = ((n1 - 273.15) * 1.8) + 32;
+
+            if (escala.equals("C") || escala.equals("c")) {
+                System.out.println("---------------------");
+                System.out.printf(">>> A temperatura em F é %.1f°\n", ctof);
+                System.out.println("---------------------");
+                System.out.printf(">>> A temperatura em K é %.1f°\n", ctok);
+                System.out.println("---------------------");
+            } else if (escala.equals("F") || escala.equals("f")) {
+                System.out.println("---------------------");
+                System.out.printf(">>> A temperatura em C é %.1f°\n", ftoc);
+                System.out.println("---------------------");
+                System.out.printf(">>> A temperatura em K é %.1f°\n", ftok);
+                System.out.println("---------------------");
+            } else if (escala.equals("K") || escala.equals("k")) {
+                System.out.println("---------------------");
+                System.out.printf(">>> A temperatura em C é %.1f°\n", ktoc);
+                System.out.println("---------------------");
+                System.out.printf(">>> A temperatura em F é %.1f°\n", ktof);
+                System.out.println("---------------------");
+            } else {
+                System.out.println(">>> Repita o processo por favor, a escala está incorreta.");
+            }
+
+            System.out.println("Deseja continuar (s/n): ");
+            repetir = scan.next();
+            if (repetir.equals("n")) {
+                System.out.println("Finalizado aqui...");
+                fechado = true;
+            }
+        } while (fechado == false);
+        scan.close();
+    }
+}
